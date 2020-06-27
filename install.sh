65273cb257d6c7e596202174519faa806144fb5c
Walter Cervini <wcervini@gmail.com>
2020-06-27 22:40:58 +0100

    Change on instalation script

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ set -e
 
 cd ~/.vim
 cat ~/.vim/myvim.vim > ~/.vimrc
-echo "Installed the MyVim configuration successfully! Enjoy :-)"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall 
+echo "Installed the MyVim configuration successfully! Enjoy :-)"wq
+
