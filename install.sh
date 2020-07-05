49a94117cfea74e2073dd0ecbeaa9e1604d7af39
Walter Cervini <wcervini@gmail.com>
2020-07-05 22:17:03 +0100

    Add :bash_aliases got git aliases & functions

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ set -e
 
 #cd ~/.vim
 cat ~/myvim/myvim.vim > ~/.vimrc
+cp ~/myvim/.bashaliases > ~/.bash_aliases
+source ~/.bash_aliases
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall 
+vim +PluginInstall +qall
 echo "Installed the MyVim configuration successfully! Enjoy :-)"wq
 
