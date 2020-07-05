91e587680a0bd2fd5b99c6bf0f0137b463859e8a
Walter Cervini <v0lp@servigarantia.es>
2020-07-05 10:09:22 +0100

    Modificacion script de Instalacion, no conseguia la ruta

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-cd ~/.vim
-cat ~/.vim/myvim.vim > ~/.vimrc
+#cd ~/.vim
+cat ~/myvim/myvim.vim > ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall 
 echo "Installed the MyVim configuration successfully! Enjoy :-)"wq
