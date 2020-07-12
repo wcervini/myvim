dbab1020a96d0a87cb5a64d3e9d7d4bf1b584518
Walter Cervini <wcervini@gmail.com>
2020-07-12 14:20:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ set -e
 
 #cd ~/.vim
 cat ~/myvim/myvim.vim > ~/.vimrc
-cp ~/myvim/.bashaliases > ~/.bash_aliases
-source ~/.bash_aliases
+cat ~/myvim/.bashaliases > ~/.bash_aliases
+#source ~/.bash_aliases
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 echo "Installed the MyVim configuration successfully! Enjoy :-)"wq
